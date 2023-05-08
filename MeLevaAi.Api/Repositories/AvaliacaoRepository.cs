acbc7d037672c448d297675a82c76123169a4216
Christian Diemer <DiemerC@delagelanden.com>
2023-05-08 16:25:04 -0300

    ajustes

--- MeLevaAi.Api/Repositories/AvaliacaoRepository.cs
+++ MeLevaAi.Api/Repositories/AvaliacaoRepository.cs
@@ -2,17 +2,9 @@
 
 namespace MeLevaAi.Api.Repositories
 {
-  public class AvaliacaoRepository
-  {
-    private static readonly List<Avaliacao> _avaliacoes = new();
-
-    public Avaliacao ObterPorCorridaEUsuarioAvaliado(Guid id, Guid usuarioAvaliado)
-        => _avaliacoes.Where(a => a.CorridaId == id && a.UsuarioAvaliacaoId == usuarioAvaliado).FirstOrDefault();
-
-
-    public Avaliacao Cadastrar(Avaliacao avaliacao)
+    public class AvaliacaoRepository
     {
-      _avaliacoes.Add(avaliacao);
+        private static readonly List<Avaliacao> _avaliacoes = new();
 
         public Avaliacao ObterPorCorridaEUsuarioAvaliado(Guid id, Guid usuarioAvaliado)
             => _avaliacoes.Where(a => a.CorridaId == id && a.UsuarioAvaliacaoId == usuarioAvaliado).FirstOrDefault();
@@ -25,5 +17,4 @@ namespace MeLevaAi.Api.Repositories
             return avaliacao;
         }
     }
-  }
 }
